f800e9b00eedcc4313e96af550c2dfceeceb901d
Marufenko <price.of.the.flight@hotmail.com>
2016-06-11 19:41:40 +0300

    Create L3 Problem 5b.py

---
+++ L3 Problem 5b.py
@@ -0,0 +1,14 @@
+'''
+Convert the following code into code that uses a for loop.
+
+print "Hello!"
+print 10
+print 8
+print 6
+print 4
+print 2
+'''
+
+print('Hello!')
+for a in range(10,1,-2):
+  print(a)
