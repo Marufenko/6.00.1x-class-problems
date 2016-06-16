00ca9d075129571b6649653ec4976284d18b1873
Marufenko <price.of.the.flight@hotmail.com>
2016-06-16 22:30:52 +0300

    Delete L4 Problem 8.py

--- L4 Problem 8.py
+++
@@ -1,7 +0,0 @@
-def fourthPower(x):
-    '''
-    function that takes in one number and returns that value raised to the fourth power. 
-    x: int or float.
-    '''
-    result = square(square(x))
-    return result
