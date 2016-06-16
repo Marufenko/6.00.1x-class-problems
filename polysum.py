1cd90af7dd7c5ad614f87dab4ab429a70d75c48f
Marufenko <price.of.the.flight@hotmail.com>
2016-06-16 22:28:48 +0300

    Update polysum.py

--- polysum.py
+++ polysum.py
@@ -5,6 +5,6 @@ def polysum(n,s):
     This function should sum the area and square of the perimeter of the regular polygon. 
     '''
     area = abs((0.25*n*s**2)/(math.tan(math.pi/n)))
-    square = (n * s)**2
-    sum = area + square
+    perimeter = (n * s)
+    sum = area + perimeter**2
     return round(sum,4)
